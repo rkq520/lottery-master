dc62996d8e71e033ac8313ced37c356aabd24f2f
RKQ <RKQ>
2016-09-27 20:14:44 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# lottery
+<p>一个彩票查询App, 数据来源于360和网络.</p>
+<img src="https://raw.githubusercontent.com/feimengwang/lottery/master/pic/first.png" width="200px"/>
+<img src="https://raw.githubusercontent.com/feimengwang/lottery/master/pic/second.png" width="200px"/>
+<img src="https://raw.githubusercontent.com/feimengwang/lottery/master/pic/third.png" width="200px"/>
+
+
+<p>程序有很多不足的地方。正在改善中。。。</p>
+#2016/4/6
+1, 实现了Translucent System Bar</br>
+2, 优化了程序跳转和退出问题 </br>
+3, 实现了检查新版本</br>
---
+++ app/src/main/java/cn/true123/lottery/retrofit/LotteryService.java
@@ -0,0 +1,27 @@
+package cn.true123.lottery.retrofit;
+
+
+
+import cn.true123.lottery.model.ApiVersion;
+import cn.true123.lottery.model.Lottery;
+import cn.true123.lottery.model.LotteryDetail;
+import cn.true123.lottery.model.LotteryHistory;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+import rx.Observable;
+
+/**
+ * Created by feimeng0530 on 2016/3/17.
+ */
+public interface LotteryService {
+
+    @GET("qkaijiang?r=1458219747840")
+    Observable<Lottery> geLastData360();
+
+    @GET("qkj")
+    Observable<LotteryDetail> getLotteryDetail(@Query("lotId") String lotId, @Query("issue") String issue);
+    @GET("qkjlist")
+    Observable<LotteryHistory> geLotteryHistory(@Query("lotId") String lotId, @Query("page") String page);
+    @GET("57051b47748aac1c1a000036")
+   Observable<ApiVersion> getLastVersion(@Query("api_token") String api_token);
+}
